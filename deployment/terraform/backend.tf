85f7d17821a2fe4d31c1b517191815956959cd70
Dave Wang <wangdave@google.com>
2026-02-15 19:09:09 +0000

    fix: renamed repository references to a2a-multiagent-langgraph-cicd

--- deployment/terraform/backend.tf
+++ deployment/terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
     bucket = "dw-genai-prod-terraform-state"
-    prefix = "a2a-on-ae-multiagent-langgraph/prod"
+    prefix = "a2a-multiagent-langgraph-cicd/prod"
   }
 }
